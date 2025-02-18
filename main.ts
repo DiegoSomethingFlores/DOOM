08d16ab5a7ebc8e2e9fa1bc5863aa3c1ccc6202a
DiegoSomethingFlores <diego.ramosflores@Student.csd509j.net>
2025-02-18 13:54:05 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+tiles.setCurrentTilemap(tilemap`level2`)
+let mySprite2 = Render.getRenderSpriteVariable()
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
